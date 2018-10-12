0df546d9b8fe978c1dab6cd26c4359406706b26e
Boris Aagatić <boris.agatic0@gmail.com>
2018-10-12 19:52:02 +0200

    friday update

--- wp-content/plugins/algebra-lab/index.php
+++ wp-content/plugins/algebra-lab/index.php
@@ -181,5 +181,30 @@ if (is_admin()) {
 add_action('get_header', 'enable_threaded_comments');
 
 
+//username in the menu
+function give_profile_name(){
+    $user=wp_get_current_user();
+    $name=$user->user_firstname;
+    return "Bok, " .$name;
+}
+
+add_shortcode('profile_name', 'give_profile_name');
+
+add_filter( 'wp_nav_menu_objects', 'my_dynamic_menu_items' );
+function my_dynamic_menu_items( $menu_items ) {
+    foreach ( $menu_items as $menu_item ) {
+        if ( '#profile_name#' == $menu_item->title ) {
+            global $shortcode_tags;
+            if ( isset( $shortcode_tags['profile_name'] ) ) {
+                // Or do_shortcode(), if you must.
+                $menu_item->title = call_user_func( $shortcode_tags['profile_name'] );
+            }
+        }
+    }
+
+    return $menu_items;
+}
+
+
 
  ?>
